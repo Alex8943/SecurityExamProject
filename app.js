cc23e7310ea7b343effb3241fa4ede26b26dcce6
Alex8943 <65241585+Alex8943@users.noreply.github.com>
2023-04-23 15:42:22 +0200

    My enite login logout application workds

--- app.js
+++ app.js
@@ -1,54 +1,104 @@
+if (process.env.NODE_ENV !== 'production') {
+    const dotenv = await import('dotenv');
+    dotenv.config();
+}
+  
+
 import express from 'express'; 
-const app = express(); 
 import bcrypt from 'bcrypt';
+import flash from 'express-flash';
+import session from 'express-session';
+import passport from 'passport';
+import methodOverride from 'method-override';
+
+const app = express(); 
+
+import initializePassport from './passport-config.js';
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email), 
+    id => users.find(user => user.id === id)
+
+); 
+
+const users = [];
+
 
 app.set("view-engine", "ejs")
 app.use(express.urlencoded({ extended: false }));
-//app.use ... urlencoded mehod lets us use the values from the forms in our app.js file (req.body.name)
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false, 
+}))
 
-const users = [];
-//This array lets us store the users in an array, insted of a database
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 
-app.get("/", (req, res) => {
-    res.render("index.ejs", { name: "Alex" });
+
+
+app.get("/", checkAuthenticated, (req, res) => {
+    res.render("index.ejs", { name: req.user.name });
 });
 
 
-app.get("/login", (req, res) => {
+app.get("/login", checkNotAuthenticated, (req, res) => {
     res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-    
-});
+app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
+     successRedirect: '/',
+     failureRedirect: '/login',
+     failureFlash: true
+}));
 
-app.get("/register", (req, res) => {
+app.get("/register", checkNotAuthenticated, (req, res) => {
     res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {
+app.post("/register", checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-            users.push({ //user.push is used to add a new user to the array
-                id: Date.now().toString(), //Unique id for each user
-                name: req.body.name, //req.body.name is used to get the name from the form
-                email: req.body.email, //the same goes for the email
-                password: hashedPassword //the password is hashed with bcrypt
+            users.push({ 
+                id: Date.now().toString(), 
+                name: req.body.name, 
+                email: req.body.email, 
+                password: hashedPassword 
             });
-            res.redirect("/login"); //IF the user is sucessfully registered, we redirect him to the login page
+            res.redirect("/login"); 
 
     } catch (error) {
         res.redirect("/register");
-        console.log("Somnething went wrong: " + error);
     }
     console.log(users);
-    
 });
 
-//this tryy catch block is used to make sure we registere the user with the correct hashed password (bcrypt)
+app.delete('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { 
+        return next(err); 
+    }
+      res.redirect('/login');
+    });
+});
 
 
 
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next() //Everything is ok, continue
+    }
+        res.redirect('/login')
+};
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+        next()
+};
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
